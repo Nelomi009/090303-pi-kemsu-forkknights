fb9cd7a8a9a2eb38edb4cd898c8d0813453d0979
Konstantin <H1dra5@yandex.ru>
2026-03-27 09:01:04 +0700

    Я начал добавлять своего героя, добавил ему немного предисторию

---
+++ Group3/Konovalov.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkKnights.Group3 {
+  public class Konovalov : IKnight {
+    string name, power, fromUniverse, species, status, isHeAHero;
+    int age;
+  }
+
+  public Konovalov() {
+    name = "Сифил Лифисов";
+    power = "Изначально был рождён с очень сильной магией огня, из-за чего сошёл с ума";
+    fromUniverse = "Выходец из вселенной, в которой правят драконы и царит средневековье";
+    species = "Человек, рождённый в сливянии дракона и человека";
+    }
+}
